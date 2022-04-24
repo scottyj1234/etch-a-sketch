6106c5b7ca11dbdc507426212e45845b233f5dc4
scottyj1234 <scottysoccer30@gmail.com>
2022-04-24 16:07:23 -0500

    Add ability to change grid size

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,4 +1,5 @@
 const gridContainer = document.querySelector('.grid-container');
+let gridSize = 4;
 
 function drawGrid(numSquares) {
     for (let row = 1; row <= numSquares; ++row){
@@ -19,8 +20,41 @@ function drawGrid(numSquares) {
     );
 }
 
+function eraseGrid() {
+    while (gridContainer.firstChild){
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+}
+
 function colorSquare() {
     this.classList.add('visited');
 }
 
-drawGrid(4);
+function changeSize() {
+    let newGridSize = window.prompt("How many squares per side?", gridSize.toString());
+    if (!newGridSize) return;
+
+    while (Number.isNaN(newGridSize) || Number.parseInt(newGridSize) < 0 || Number.parseInt(newGridSize) > 100){
+        if (Number.isNaN(newGridSize)) {
+            newGridSize = window.prompt(`${newGridSize} is not a number. Please enter a number between 0 and 100.`, gridSize.toString());
+        } else if (newGridSize < 0) {
+            newGridSize = window.prompt(`Negative number is not valid. Number must be bewtween 0 and 100.`, gridSize.toString());
+        } else {
+            newGridSize = window.prompt(`Grid size cannot exceed 100. Number must be between 0 and 100`, gridSize.toString());
+        }
+    }
+
+    newGridSize = Number.parseInt(newGridSize);
+    if (newGridSize === gridSize){
+        document.querySelectorAll('.visited').forEach(
+            (square) => square.classList.remove('visited')
+        );
+    } else {
+        eraseGrid();
+        drawGrid(newGridSize);
+        gridSize = newGridSize;
+    }
+}
+
+document.querySelector('button').addEventListener('click', changeSize)
+drawGrid(gridSize);
