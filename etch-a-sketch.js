ebfea4f5645c78a7b8a0024a8d274dfb3cc0530c
scottyj1234 <scottysoccer30@gmail.com>
2022-04-24 15:36:55 -0500

    Create draw grid function

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,22 +1,26 @@
 const gridContainer = document.querySelector('.grid-container');
 
-for (let row = 1; row <= 4; ++row){
-    let newRow = document.createElement('div');
-    newRow.classList.add('row');
-    newRow.id = `row${row}`;
-    for (let col = 1; col <= 4; ++col){
-        let newSquare = document.createElement('div');
-        newSquare.classList.add('square');
-        newSquare.id = `square${row - 1 + col}`;
-        newRow.appendChild(newSquare);
+function drawGrid(numSquares) {
+    for (let row = 1; row <= numSquares; ++row){
+        let newRow = document.createElement('div');
+        newRow.classList.add('row');
+        newRow.id = `row${row}`;
+        for (let col = 1; col <= numSquares; ++col){
+            let newSquare = document.createElement('div');
+            newSquare.classList.add('square');
+            newSquare.id = `square${row - 1 + col}`;
+            newRow.appendChild(newSquare);
+        }
+        gridContainer.appendChild(newRow);
     }
-    gridContainer.appendChild(newRow);
+
+    document.querySelectorAll('.square').forEach(
+        (square) => square.addEventListener('mouseover', colorSquare)
+    );
 }
 
 function colorSquare() {
     this.classList.add('visited');
 }
 
-document.querySelectorAll('.square').forEach(
-    (square) => square.addEventListener('mouseover', colorSquare)
-);
+drawGrid(4);
