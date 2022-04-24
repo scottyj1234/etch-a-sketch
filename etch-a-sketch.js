8e2a5a8d3144115c32ed2363c60e9625bbf5f502
scottyj1234 <scottysoccer30@gmail.com>
2022-04-24 15:21:17 -0500

    Create 4x4 div squares

---
+++ etch-a-sketch.js
@@ -0,0 +1,14 @@
+const gridContainer = document.querySelector('.grid-container');
+
+for (let row = 1; row <= 4; ++row){
+    let newRow = document.createElement('div');
+    newRow.classList.add('row');
+    newRow.id = `row${row}`;
+    for (let col = 1; col <= 4; ++col){
+        let newSquare = document.createElement('div');
+        newSquare.classList.add('square');
+        newSquare.id = `square${row - 1 + col}`;
+        newRow.appendChild(newSquare);
+    }
+    gridContainer.appendChild(newRow);
+}
